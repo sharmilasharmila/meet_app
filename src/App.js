5729b1cfe55a44d52b37cd145237066acef471d9
sharmilasharmila <sra084@post.uit.no>
2021-05-26 06:54:17 +0530

    End

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import "./nprogress.css";
 import {
   ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer
 } from 'recharts';
+import EventGenre from './EventGenre';
 
 class App extends Component {
   state = {
@@ -62,22 +63,25 @@ class App extends Component {
           updateEvents={this.updateEvents}
           numberOfEvents={numberOfEvents}
         />       
-         <h4>Events in each city</h4>
-
-        <ResponsiveContainer height={400} >
-          <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
-            <CartesianGrid />
-            <XAxis type="category" dataKey="city" name="city" />
-            <YAxis
-              allowDecimals={false}
-              type="number"
-              dataKey="number"
-              name="number of events"
-            />
-            <Tooltip cursor={{ strokeDasharray: "3 3" }} />
-            <Scatter data={this.getData()} fill="#8884d8" />
-          </ScatterChart>
-        </ResponsiveContainer>
+        <div>
+          <EventGenre events={events}/>
+          <h4>Events in each city</h4>
+          <ResponsiveContainer height={400} >
+            <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+              <CartesianGrid />
+              <XAxis type="category" dataKey="city" name="city" />
+              <YAxis
+                allowDecimals={false}
+                type="number"
+                dataKey="number"
+                name="number of events"
+              />
+              <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+              <Scatter data={this.getData()} fill="#8884d8" />
+            </ScatterChart>
+          </ResponsiveContainer>
+        </div>
+        
         <EventList events={events} />
       </div>
     );
---
+++ src/EventGenre.js
@@ -0,0 +1,45 @@
+import React, {useEffect, useState} from 'react';
+import {
+    PieChart, Pie, Cell, ResponsiveContainer
+} from 'recharts';
+
+
+
+const EventGenre = ({ events }) => {
+    const [data, setData] = useState([]);
+    const getData = () => {
+        const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'Angular'];
+        const data = genres.map((genre) => {
+          const value = events.filter(({ summary }) => summary.split(' ').includes(genre)).length;
+          return { name: genre, value };
+        })
+      return data;
+    };
+
+    useEffect(() => { 
+        setData(() => getData());
+    }, [events]); 
+
+    const colors = ['#000ab4', '#c1000a', '#e300f2', '#23d900', '#000308']
+
+    return (
+        <ResponsiveContainer height={400} >
+            <PieChart width={400} height={400} margin={{left: 350}}>
+                <Pie
+                    data={data}
+                    cx={200}
+                    cy={200}
+                    labelLine={false}
+                    outerRadius={80}
+                    fill="#8884d8"
+                    dataKey="value"
+                    label={({ name, percent}) => `${name} ${(percent * 100).toFixed(0)}%`}
+                    >
+                    {data.map((entry, index) => <Cell key={`cell-${index}`} fill={colors[index % colors.length]} name={entry.name}/>)}
+                    </Pie>
+            </PieChart>
+        </ResponsiveContainer>
+    )
+}
+
+export default EventGenre;
